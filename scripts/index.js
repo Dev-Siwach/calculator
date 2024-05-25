5517fcaf725a3c5f29ae4e238a97305778ae82be
Dev Siwach <dev652007@proton.me>
2024-05-25 12:03:36 +0530

    Started working on script

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const numberOne = document.querySelector("#numberOne");
+const numberTwo = document.querySelector("#numberTwo");
+const numberThree = document.querySelector("#numberThree");
+const numberFour = document.querySelector("#numberFour");
+const numberFive = document.querySelector("#numberFive");
+const numberSix = document.querySelector("#numberSix");
+const numberSeven = document.querySelector("#numberSeven");
+const numberEight = document.querySelector("#numberEight");
+const numberNine = document.querySelector("#numberNine");
+const numberZero = document.querySelector("#numberZero");
+
+const expression = document.querySelector("#expression");
+const userExpressionInput =
+
+
